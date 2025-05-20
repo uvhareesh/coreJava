8cc3a9e434651c936668391e72c3dbb8d15c27ba
uvhareesh <uvharish90@gmail.com>
2025-05-20 23:07:15 +0530

    Do while loop program for 1...10 numbers display

---
+++ src/loopingORIterativeStatements/DowhileLoopo.java
@@ -0,0 +1,18 @@
+package loopingORIterativeStatements;
+
+public class DowhileLoopo {
+
+	public static void main(String[] args) {
+
+		// 1...10 numbers
+		
+		int i=1;
+		do 
+		{
+			System.out.println(i);
+			i++;
+		}while(i<=10);
+		
+	}
+
+}
