6185a853e404b936745fc1cb2bff2d3f059324ac
uvhareesh <uvharish90@gmail.com>
2025-05-23 19:55:06 +0530

    dowhile loop program

---
+++ src/loopingORIterativeStatements/DoWhileLoop1.java
@@ -0,0 +1,17 @@
+package loopingORIterativeStatements;
+
+public class DoWhileLoop1 {
+
+	public static void main(String[] args) {
+		
+		// 10...1 numbers
+		
+		int i=10;
+		
+		do {
+			System.out.println(i);
+			i--;
+		}while(i>0); // i>=1
+	}
+
+}
